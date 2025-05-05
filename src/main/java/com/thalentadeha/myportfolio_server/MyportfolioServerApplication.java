1bb3fa75bfad50d396ad255dca5cfb9baec2ccf0
Muhammad Thalenta Dirgantara Deha <92012703+thalentadeha@users.noreply.github.com>
2025-05-05 19:20:31 +0700

    initial commit

---
+++ src/main/java/com/thalentadeha/myportfolio_server/MyportfolioServerApplication.java
@@ -0,0 +1,13 @@
+package com.thalentadeha.myportfolio_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyportfolioServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyportfolioServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thalentadeha/myportfolio_server/MyportfolioServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thalentadeha.myportfolio_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyportfolioServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
